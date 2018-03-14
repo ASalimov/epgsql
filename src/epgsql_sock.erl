b1d318e8c0379796e75e41eed3e61a791abfd178
mac <salimov.artem@gmail.com>
2018-03-14 21:43:16 +0200

    fix

--- src/epgsql_sock.erl
+++ src/epgsql_sock.erl
@@ -122,7 +122,7 @@ init([]) ->
     {ok, #state{}};
 
 init(Args) ->
-  timer:sleep(1000),
+  timer:sleep(200),
   Settings = epgsql:to_proplist(Args),
   Host = proplists:get_value(host, Settings, "localhost"),
   Username = proplists:get_value(username, Settings, os:getenv("USER")),
